70e3a6892926e13fa361c8d3c5ed60a274b53af1
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-04 20:39:46 +0600

    added 2057A

---
+++ Codeforces solutions/2057A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define p(x) cout<<x<<endl;
+using namespace std;
+
+int main() {
+    ll t;
+    cin>>t;
+    while(t--){
+        ll a,b;
+        cin>>a>>b;
+        p(max(a,b)+1)        
+    }  
+    return 0;
+}
