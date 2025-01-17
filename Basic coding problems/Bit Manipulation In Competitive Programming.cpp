ea1b34e35c4a6061e5179202358446643aa0d26b
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-17 12:22:13 +0600

    bitmask !!

--- Basic coding problems/Bit Manipulation In Competitive Programming.cpp
+++ Basic coding problems/Bit Manipulation In Competitive Programming.cpp
@@ -6,35 +6,73 @@ void set(int &num, int &pos){
     num|=(1<<pos);
 }
 
+
 void unset(int &num, int &pos){
     num&=(~(1<<pos));
 }
 
+
 void toggle(int &num, int &pos){
     num^=(1<<pos);
 }
+
 
 bool setorunset(int &num, int &pos){
     bool bit = num & (1<<pos);
     return bit;
 }
 
+
 void onescompliment(int &num){
     num = (~num);
 }
 
+
 void twoscompliment(int &num){
     num = (~num+1);
 }
+
 
 void striplastsetbit(int &num)
 {
     num = num & (num-1);
 }
 
-int lowestsetbit(int &num){
+
+int lowestsetbit(int &num) {
+    if (num == 0) return 0;
     int res = num & (-num);
     return res;
+}
+
+
+void clearlowestbit(int &num){
+    num = num & (num-1);
+}
+
+int extractlowestbit(int &num) {
+    if (num == 0) return 0;
+    return num & (-num);
+}
+
+
+void clearsfromlsbtoith(int &num, int &i) {
+    num = num & ~((1 << (i + 1)) - 1);
+}
+
+
+void clearsfrommsbtoith(int &num, int &i) {
+    num = num & ~((1 << i) - 1);
+}
+
+
+void convertlowercase(char &c){
+    c = c|' ';
+}
+
+
+void convertuppercase(char &c){
+    c = c&'_';
 }
 
 int main(){
@@ -53,5 +91,17 @@ int main(){
     striplastsetbit(num);
     p((int)num)
     p(lowestsetbit(num))
+    clearlowestbit(num);
+    p((int)num)
+    p(extractlowestbit(num))
+    clearsfromlsbtoith(num, pos);
+    p(num)
+    clearsfrommsbtoith(num,pos);
+    p(num)
+    char c = 'A';
+    convertlowercase(c);
+    p(c)
+    convertuppercase(c);
+    p(c)
     return 0;
 }
