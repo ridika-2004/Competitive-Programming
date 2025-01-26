5a3dfc073e2cea68f8d082e1e004b42f93183d35
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-01-26 20:59:16 +0600

    added 2062A

---
+++ Codeforces solutions/2062A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define p(x) cout<<x<<endl;
+using namespace std;
+
+int main(){
+    ll t;
+    cin>>t;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        ll n = count(s.begin(), s.end(), '1');
+        p(n)
+    }
+    
+    return 0;
+}
