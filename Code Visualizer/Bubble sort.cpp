a5a81bffc33932a2e23d4c931ef3da1cd3806472
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-02-27 19:54:07 +0600

    added bubble sort

---
+++ Code Visualizer/Bubble sort.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<thread>
+#define ll long long
+#define p(x) cout<<x<<endl;
+using namespace std;
+
+int main(){
+
+    ll n;
+    cin>>n;
+    vector<ll> v(n);
+    for(ll i=0; i<n; i++){
+        cin>>v[i];
+    }
+    for(ll i=0; i<n; i++){
+        for(ll j=0; j<n-i-1; j++){
+            if(v[j]>v[j+1]){
+                swap(v[j], v[j+1]);
+            }
+        }
+
+        for(ll i=0; i<n; i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    return 0;
+}
