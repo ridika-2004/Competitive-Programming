99fe8470a9dd12f60ff1fb2864cc324af16d38ee
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-02-28 20:47:02 +0600

    added 2071A

---
+++ Codeforces solutions/2071A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define p(x) cout<<x<<endl;
+using namespace std;
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        if(n%3==1){
+            p("Yes")
+        } else {
+            p("No")
+        }
+    }
+    return 0;
+}
