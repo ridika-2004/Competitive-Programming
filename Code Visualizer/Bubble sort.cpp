3b54c6d5f2f9fb55a8c9216d989c27c2a75f9b1f
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-03-03 17:10:07 +0600

    added refined bubble sort

--- Code Visualizer/Bubble sort.cpp
+++ Code Visualizer/Bubble sort.cpp
@@ -1,31 +1,50 @@
-#include<bits/stdc++.h>
-#include<thread>
+#include <bits/stdc++.h>
+#include <thread>
 #define ll long long
-#define p(x) cout<<x<<endl;
+#define p(x) cout << x << endl;
 using namespace std;
 
-int main(){
-
-    ll n;
-    cin>>n;
-    vector<ll> v(n);
-    for(ll i=0; i<n; i++){
-        cin>>v[i];
+void print(vector<ll> v) {
+    cout << "Current Array: [ ";
+    for (ll i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
     }
-    for(ll i=0; i<n; i++){
-        for(ll j=0; j<n-i-1; j++){
-            if(v[j]>v[j+1]){
-                swap(v[j], v[j+1]);
-                cout<<"Swapping "<<v[j]<<" and "<<v[j+1]<<endl;
-                this_thread::sleep_for(chrono::seconds(1));
+    cout << "]" << endl;
+    cout << "-----------------------------" << endl;
+}
+
+int main() {
+    ll n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    vector<ll> v(n);
+    cout << "Enter the elements:\n";
+    for (ll i = 0; i < n; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> v[i];
+    }
+
+    cout << "\nStarting Bubble Sort Animation...\n";
+    print(v);
+    this_thread::sleep_for(chrono::seconds(2));
+
+    for (ll i = 0; i < n; i++) {
+        for (ll j = 0; j < n - i - 1; j++) {
+            if (v[j] > v[j + 1]) {
+                cout << "⚡ Swapping: " << v[j] << " ↔ " << v[j + 1] << endl;
+                swap(v[j], v[j + 1]);
+
+                this_thread::sleep_for(chrono::seconds(2)); // Delay to visualize swap
+                print(v);
+                this_thread::sleep_for(chrono::seconds(3)); // Extra delay
             }
         }
-
-        for(ll i=0; i<n; i++){
-            cout<<v[i]<<" ";
-        }
-        cout<<endl;
-        this_thread::sleep_for(chrono::seconds(1));
+        cout << "✔ Pass " << i + 1 << " completed!\n";
     }
+
+    cout << "🎉 Sorting Completed! Final Sorted Array:\n";
+    print(v);
+
     return 0;
 }
