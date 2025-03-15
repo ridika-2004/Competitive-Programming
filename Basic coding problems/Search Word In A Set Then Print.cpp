a440cb95e7ee82a7d7e7f760ff35f7f5995bb82c
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-03-15 22:47:11 +0600

    idk

---
+++ Basic coding problems/Search Word In A Set Then Print.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define p(x) cout << x <<endl;
+#define m(x) cout << x <<" ";
+using namespace std;
+
+void printword(set<string> dict, string s){
+    ll count=0;
+    bool found=0;
+    for(auto i: dict){
+        if(i.find(s)==0 && count<3){
+            found =1;
+            m(i)
+            count++;
+        }
+    }
+    //p("")
+    if(!found){
+        p("null")
+        return;
+    }
+}
+
+int main(){
+
+    ll n;
+    cin>>n;
+    set<string> dict;
+    for(ll i=0;i<n;i++){
+        string s;
+        cin>>s;
+        dict.insert(s);
+    }
+
+    string word;
+    cin>>word;
+
+    for(ll i=0;i<word.size();i++){
+        //m(word.substr(0,i+1))
+        printword(dict,word.substr(0, i+1));
+    }
+    return 0;
+}
