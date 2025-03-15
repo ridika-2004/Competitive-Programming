6d315cd03055b6f12be4be5bb32878cb00a08b0f
Ridika Naznin <168357177+ridika-2004@users.noreply.github.com>
2025-03-15 21:58:29 +0600

    added heap with priority queue

---
+++ Basic coding problems/Find Top 3 Richest Person's Net Worth's Product (With Heap).cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define p(x) cout << x <<endl;
+using namespace std;
+
+int main(){
+
+    ll n;
+    cin>>n;
+    priority_queue<ll> pq;
+    for(ll i=0;i<n;i++){
+        ll a;
+        cin>>a;
+        pq.push(a);
+        if(i==0 || i==1){
+            p(-1)
+        } else {
+            priority_queue<ll> pqcopy = pq;
+            ll x1 = pqcopy.top();
+            pqcopy.pop();
+            ll x2 = pqcopy.top();
+            pqcopy.pop();
+            ll x3 = pqcopy.top();
+            pqcopy.pop();
+
+            p(x1*x2*x3)
+        }
+    }
+
+
+    return 0;
+}
